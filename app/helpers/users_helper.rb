2be8791bf4386cdbbf875efcd87afef2e56d5daa
paulotijero <paulotijero@gmail.com>
2019-06-29 14:59:56 -0500

    Add user_list helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
   def fullname(user)
     "#{user.first_name} #{user.last_name}"
   end
+
+  def users_list(list)
+    content_tag :ul do
+      list.collect do |user|
+        concat content_tag :li, link_to(fullname(user), user)
+      end
+    end
+  end
 end
