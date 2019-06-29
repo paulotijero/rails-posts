e9a2d7639c907a64d0d6b231a66a14bdadc10499
paulotijero <paulotijero@gmail.com>
2019-06-29 14:58:26 -0500

    Refactor decorators

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,9 +1,13 @@
 class UserDecorator < SimpleDelegator
+  def define_email
+    if private_email
+      "Private"
+    else
+      email
+    end
+  end
+
   def birthday_format
     birthday.strftime('%d - %m - %Y')
   end
-
-  def define_email
-    private_email ? "Private" : email
-  end
-end
+end 
